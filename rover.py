c668576a5ebd5245e0c60ed4efb8b59caf71f791
unset <unset>
2018-09-06 15:02:28 -0400

    Solution to Curiosity rover exercise.

---
+++ rover.py
@@ -0,0 +1,10 @@
+# CMPT 120L 113
+# Mars Rover Program
+# Author: Libby Foley
+# Created: 6 Sep 2018
+
+SpeedOfLight = 186000
+Distance = 34000000
+
+print ("It will take" , Distance/SpeedOfLight , "seconds")
+
