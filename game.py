e42d83d6f57e5b58de0a9c0113265b1adb6a3616
Libby <Elizabeth.Foley1@marist.edu>
2018-09-19 17:26:07 -0400

    Added Game to README file.

---
+++ game.py
@@ -0,0 +1,92 @@
+# CMPT 120L 113
+# Libby Foley
+# 20 Sep 2018
+###
+
+# show game title
+print("\nRun"
+      "\n========\n")
+
+# show game introduction
+print(" You suddenly awake. Your heart is racing. You don't know where you are."
+      " You look around and calm down. It seems you're still at your high school."
+      " But something is different. The classrooms are the same, but everything"
+      " is run down and dark. There are cobwebs everywhere, holes in the floors"
+      " and walls, and turned over desks. You decide to look around to find a"
+      " familiar face. As you walk, you notice the hallway seems too quiet,"
+      " and you realize you're the only person in the entire school. Your"
+      " heart starts to pound. You question where everyone else is. Despite"
+      " your fear, you keep walking through the dark hall. You see something"
+      " at the end of the hall. You're suddenly surrounded by a horrible smell."
+      " You use you shirt to cover your nose and look down. You jump back, you" 
+      " see blood and guts spilled out on the floor. It looks as if someone"
+      " was just killed, but you thought you were the only one in the building."
+      " Where are you? You keep staring at the decomposing body, you can't look away."
+      " Suddenly, you hear something behind you. Maybe an axe dragging on the"
+      " floor. There's laughing too. You turn around..."
+      )
+
+# prompt the user
+input("\n<Press Enter to continue...>\n")
+
+# show the current location
+print("You are in the same hallway. You have turned around, but see no one. "
+          "Everything looks different. The halls have shifted. "
+          "You are not where you were before.")
+
+# show the current score
+print("Your score is 0.")
+
+# prompt the user
+input("\n<Press Enter to continue...>\n")
+
+# show the current location
+print("You start walking down unfamiliar halls. "
+      "Sounds of laughter echo and you hear footsteps behind you. "
+      "You start running and the steps follow. "
+      "You see an empty classroom.")
+
+# show the current score
+print("Your score is 5.")
+
+# same as above
+input("\n<Press Enter to continue...>\n")
+
+# same as above
+print("You walk into the classroom, hoping you escaped. "
+      "It's dark, but there is one candle lit. "
+      "Suddenly, the door slams shut and the candle goes out.")
+
+# same as above
+print("Your score is 10.")
+
+# again...
+input("\n<Press Enter to continue...>\n")
+
+# again...
+print("It's now pitch black in the classroom. "
+      "You start to hear voices circling around you. "
+      "You feel as if you are going mad and scream in terror.")
+
+# again...
+print("Your score is 15.")
+
+# yet again...
+input("\n<Press Enter to continue...>\n")
+
+# yet again...
+print("You run for the door, stumbling over fallen desks and chairs.")
+
+# yet again...
+print("Your score is 20.")
+
+# one last time...
+input("\n<Press Enter to continue...>\n")
+
+# show game ending
+print("You pull at the door, but it won't open. "
+      "You hear the same footsteps again and freeze. Suddenly, you feel a piercing,"
+      "sharp pain in your back. The killer got you. Game over!")
+
+# show credits
+print("\nCopyright (c) 2016-2018 Matthew A Johnson, Matt.Johnson@marist.edu")
