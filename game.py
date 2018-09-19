99ce83d767eaf67c8f7d19d978862669afc236c7
Libby <Elizabeth.Foley1@marist.edu>
2018-09-19 18:02:50 -0400

    2nd Commit. Rewrote print and input statements.

--- game.py
+++ game.py
@@ -27,66 +27,65 @@ print(" You suddenly awake. Your heart is racing. You don't know where you are."
       )
 
 # prompt the user
-input("\n<Press Enter to continue...>\n")
+PressEnter = "\n<Press Enter to continue...>\n"
+input(PressEnter)
 
 # show the current location
-print("You are in the same hallway. You have turned around, but see no one. "
-          "Everything looks different. The halls have shifted. "
-          "You are not where you were before.")
+Loc1 = "You are in the same hallway. You have turned around, but see no one. Everything looks different. The halls have shifted. You are not where you were before."
+print(Loc1)
 
 # show the current score
-print("Your score is 0.")
+score1 = "Your score is 0."
+print(score1)
 
 # prompt the user
-input("\n<Press Enter to continue...>\n")
+input(PressEnter)
 
 # show the current location
-print("You start walking down unfamiliar halls. "
-      "Sounds of laughter echo and you hear footsteps behind you. "
-      "You start running and the steps follow. "
-      "You see an empty classroom.")
+Loc2 = "You start walking down unfamiliar halls. Sounds of laughter echo and you hear footsteps behind you. You start running and the steps follow. You see an empty classroom."
+print(Loc2)
 
 # show the current score
 print("Your score is 5.")
 
 # same as above
-input("\n<Press Enter to continue...>\n")
+input(PressEnter)
 
 # same as above
-print("You walk into the classroom, hoping you escaped. "
-      "It's dark, but there is one candle lit. "
-      "Suddenly, the door slams shut and the candle goes out.")
-
+Loc3 = "You walk into the classroom, hoping you escaped. It's dark, but there is one candle lit. Suddenly, the door slams shut and the candle goes out."
+print(Loc3)
 # same as above
-print("Your score is 10.")
+score2 = "Your score is 10."
+print(score2)
 
 # again...
-input("\n<Press Enter to continue...>\n")
+input(PressEnter)
 
 # again...
-print("It's now pitch black in the classroom. "
-      "You start to hear voices circling around you. "
-      "You feel as if you are going mad and scream in terror.")
+Loc4 = "It's now pitch black in the classroom. You start to hear voices circling around you. You feel as if you are going mad and scream in terror."
+print(Loc4)
 
 # again...
-print("Your score is 15.")
+score3 = "Your score is 15."
+print(score3)
 
 # yet again...
-input("\n<Press Enter to continue...>\n")
+input(PressEnter)
 
 # yet again...
-print("You run for the door, stumbling over fallen desks and chairs.")
+Loc5 = "You run for the door, stumbling over fallen desks and chairs."
+print(Loc5)
 
 # yet again...
-print("Your score is 20.")
+score4 = "Your score is 20."
+print(score4)
 
 # one last time...
-input("\n<Press Enter to continue...>\n")
+input(PressEnter)
 
 # show game ending
-print("You pull at the door, but it won't open. "
-      "You hear the same footsteps again and freeze. Suddenly, you feel a piercing,"
-      "sharp pain in your back. The killer got you. Game over!")
+Loc6 = "You pull at the door, but it won't open. You hear the same footsteps again and freeze. Suddenly, you feel a piercing, sharp pain in your back. The killer got you. Game over!"
+print(Loc6)
 
 # show credits
 print("\nCopyright (c) 2016-2018 Matthew A Johnson, Matt.Johnson@marist.edu")
